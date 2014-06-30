1ecf2afcf1ecfc7bd1f53d630557171973a2cd1d
TsukasaSugiura <t.sugiura0204@gmail.com>
2014-06-30 23:23:52 +0900

    Removed Unnecessary Processing

--- Sample/AudioBeam/AudioBeam.cpp
+++ Sample/AudioBeam/AudioBeam.cpp
@@ -63,38 +63,19 @@ int _tmain( int argc, _TCHAR* argv[] )
 		return -1;
 	}
 
-	// Open Audio Input Stream 
-	IStream* pAudioStream;
-	hResult = pAudioBeam->OpenInputStream( &pAudioStream );
-	if( FAILED( hResult ) ){
-		std::cerr << "Error : IAudioBeam::OpenInputStream()" << std::endl;
-		return -1;
-	}
-
-	static const int samplingFrequency = 16000; // Sampling Frequency 16000[Hz]
-	static const int audioBufferLength = samplingFrequency / 1000;
-
 	while( 1 ){
-		// Retrieved Audio Data
-		FLOAT audioBuffer[audioBufferLength];
-		DWORD readSize = 0;
+		// Get Angle and Confidence
+		FLOAT angle = 0.0f;
+		FLOAT confidence = 0.0f;
 
-		hResult = pAudioStream->Read( reinterpret_cast<void*>( audioBuffer ), sizeof( audioBuffer ), &readSize );
+		pAudioBeam->get_BeamAngle( &angle ); // radian [-0.872665f, 0.872665f]
+		pAudioBeam->get_BeamAngleConfidence( &confidence ); // confidence [0.0f, 1.0f]
 
-		if( readSize > 0 ){
-			// Get Angle and Confidence
-			FLOAT angle = 0.0f;
-			FLOAT confidence = 0.0f;
-
-			pAudioBeam->get_BeamAngle( &angle ); // radian [-0.872665f, 0.872665f]
-			pAudioBeam->get_BeamAngleConfidence( &confidence ); // confidence [0.0f, 1.0f]
-
-			// Show Result
-			// Convert from radian to degree : degree = radian * 180 / Pi
-			if( confidence > 0.0f ){
-				std::system( "cls" );
-				std::cout << "Angle : " << angle * 180.0f / M_PI << ", Confidence : " << confidence << std::endl;
-			}
+		// Show Result
+		// Convert from radian to degree : degree = radian * 180 / Pi
+		if( confidence > 0.5f ){
+			std::system( "cls" );
+			std::cout << "Angle : " << angle * 180.0f / M_PI << ", Confidence : " << confidence << std::endl;
 		}
 
 		// Input Key ( Exit ESC key )
@@ -103,7 +84,6 @@ int _tmain( int argc, _TCHAR* argv[] )
 		}
 	}
 
-	SafeRelease( pAudioStream );
 	SafeRelease( pAudioBeam );
 	SafeRelease( pAudioBeamList );
 	SafeRelease( pAudioSource );
