a8c55e54aceabc812c92b4ba817eb1998a875612
TsukasaSugiura <t.sugiura0204@gmail.com>
2014-08-23 05:03:10 +0900

    Operate Correctly

--- Sample/Face/Face.cpp
+++ Sample/Face/Face.cpp
@@ -9,6 +9,9 @@
 #include <Kinect.Face.h>
 #include <opencv2/opencv.hpp>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 
 template<class Interface>
 inline void SafeRelease( Interface *& pInterfaceToRelease )
@@ -17,6 +20,21 @@ inline void SafeRelease( Interface *& pInterfaceToRelease )
 		pInterfaceToRelease->Release();
 		pInterfaceToRelease = NULL;
 	}
+}
+
+// Quote from Kinect for Windows SDK v2.0 Developer Preview - Samples/Native/FaceBasics-D2D, and Partial Modification
+// ExtractFaceRotationInDegrees is: Copyright (c) Microsoft Corporation. All rights reserved.
+inline void ExtractFaceRotationInDegrees( const Vector4* pQuaternion, int* pPitch, int* pYaw, int* pRoll )
+{
+	double x = pQuaternion->x;
+	double y = pQuaternion->y;
+	double z = pQuaternion->z;
+	double w = pQuaternion->w;
+
+	// convert face rotation quaternion to Euler angles in degrees
+	*pPitch = static_cast<int>( std::atan2( 2 * ( y * z + w * x ), w * w - x * x - y * y + z * z ) / M_PI * 180.0f );
+	*pYaw = static_cast<int>( std::asin( 2 * ( w * y - x * z ) ) / M_PI * 180.0f );
+	*pRoll = static_cast<int>( std::atan2( 2 * ( x * y + w * z ), w * w + x * x - y * y - z * z ) / M_PI * 180.0f );
 }
 
 int _tmain( int argc, _TCHAR* argv[] )
@@ -53,11 +71,24 @@ int _tmain( int argc, _TCHAR* argv[] )
 		return -1;
 	}
 
-	IFaceFrameSource* pFaceSource;
-	hResult = CreateFaceFrameSource( pSensor, NULL, FaceFrameFeatures::FaceFrameFeatures_LeftEyeClosed | FaceFrameFeatures::FaceFrameFeatures_RightEyeClosed, &pFaceSource );
-	if( FAILED( hResult ) ){
-		std::cerr << "Error : CreateFaceFrameSource" << std::endl;
-		return -1;
+	IFaceFrameSource* pFaceSource[BODY_COUNT];
+	DWORD features = FaceFrameFeatures::FaceFrameFeatures_BoundingBoxInColorSpace
+				   | FaceFrameFeatures::FaceFrameFeatures_PointsInColorSpace
+				   | FaceFrameFeatures::FaceFrameFeatures_RotationOrientation
+				   | FaceFrameFeatures::FaceFrameFeatures_Happy
+				   | FaceFrameFeatures::FaceFrameFeatures_RightEyeClosed
+				   | FaceFrameFeatures::FaceFrameFeatures_LeftEyeClosed
+				   | FaceFrameFeatures::FaceFrameFeatures_MouthOpen
+				   | FaceFrameFeatures::FaceFrameFeatures_MouthMoved
+				   | FaceFrameFeatures::FaceFrameFeatures_LookingAway
+				   | FaceFrameFeatures::FaceFrameFeatures_Glasses
+				   | FaceFrameFeatures::FaceFrameFeatures_FaceEngagement;
+	for( int count = 0; count < BODY_COUNT; count++ ){
+		hResult = CreateFaceFrameSource( pSensor, 0, features, &pFaceSource[count] );
+		if( FAILED( hResult ) ){
+			std::cerr << "Error : CreateFaceFrameSource" << std::endl;
+			return -1;
+		}
 	}
 
 	// Reader
@@ -75,11 +106,13 @@ int _tmain( int argc, _TCHAR* argv[] )
 		return -1;
 	}
 
-	IFaceFrameReader* pFaceReader;
-	hResult = pFaceSource->OpenReader( &pFaceReader );
-	if( FAILED( hResult ) ){
-		std::cerr << "Error : IFaceFrameSource::OpenReader()" << std::endl;
-		return -1;
+	IFaceFrameReader* pFaceReader[BODY_COUNT];
+	for( int count = 0; count < BODY_COUNT; count++ ){
+		hResult = pFaceSource[count]->OpenReader( &pFaceReader[count] );
+		if( FAILED( hResult ) ){
+			std::cerr << "Error : IFaceFrameSource::OpenReader()" << std::endl;
+			return -1;
+		}
 	}
 
 	// Description
@@ -97,17 +130,28 @@ int _tmain( int argc, _TCHAR* argv[] )
 	unsigned int bufferSize = width * height * 4 * sizeof( unsigned char );
 
 	cv::Mat bufferMat( height, width, CV_8UC4 );
-	cv::Mat bodyMat( height / 2, width / 2, CV_8UC4 );
+	cv::Mat faceMat( height / 2, width / 2, CV_8UC4 );
 	cv::namedWindow( "Face" );
 
 	// Color Table
-	cv::Vec3b color[6];
+	cv::Vec3b color[BODY_COUNT];
 	color[0] = cv::Vec3b( 255, 0, 0 );
 	color[1] = cv::Vec3b( 0, 255, 0 );
 	color[2] = cv::Vec3b( 0, 0, 255 );
 	color[3] = cv::Vec3b( 255, 255, 0 );
 	color[4] = cv::Vec3b( 255, 0, 255 );
 	color[5] = cv::Vec3b( 0, 255, 255 );
+
+	// Face Property Table
+	std::string property[FaceProperty::FaceProperty_Count];
+	property[0] = "Happy";
+	property[1] = "Engaged";
+	property[2] = "WearingGlasses";
+	property[3] = "LeftEyeClosed";
+	property[4] = "RightEyeClosed";
+	property[5] = "MouthOpen";
+	property[6] = "MouthMoved";
+	property[7] = "LookingAway";
 
 	// Coordinate Mapper
 	ICoordinateMapper* pCoordinateMapper;
@@ -118,17 +162,19 @@ int _tmain( int argc, _TCHAR* argv[] )
 	}
 
 	while( 1 ){
-		// Frame
+		// Color Frame
 		IColorFrame* pColorFrame = nullptr;
 		hResult = pColorReader->AcquireLatestFrame( &pColorFrame );
 		if( SUCCEEDED( hResult ) ){
 			hResult = pColorFrame->CopyConvertedFrameDataToArray( bufferSize, reinterpret_cast<BYTE*>( bufferMat.data ), ColorImageFormat_Bgra );
 			if( SUCCEEDED( hResult ) ){
-				cv::resize( bufferMat, bodyMat, cv::Size(), 0.5, 0.5 );
+				cv::resize( bufferMat, faceMat, cv::Size(), 0.5, 0.5 );
 			}
 		}
 		SafeRelease( pColorFrame );
 
+		// Body Frame
+		cv::Point point[BODY_COUNT];
 		IBodyFrame* pBodyFrame = nullptr;
 		hResult = pBodyReader->AcquireLatestFrame( &pBodyFrame );
 		if( SUCCEEDED( hResult ) ){
@@ -139,10 +185,10 @@ int _tmain( int argc, _TCHAR* argv[] )
 					BOOLEAN bTracked = false;
 					hResult = pBody[count]->get_IsTracked( &bTracked );
 					if( SUCCEEDED( hResult ) && bTracked ){
+						/*// Joint
 						Joint joint[JointType::JointType_Count];
 						hResult = pBody[count]->GetJoints( JointType::JointType_Count, joint );
 						if( SUCCEEDED( hResult ) ){
-							// Joint
 							for( int type = 0; type < JointType::JointType_Count; type++ ){
 								ColorSpacePoint colorSpacePoint = { 0 };
 								pCoordinateMapper->MapCameraPointToColorSpace( joint[type].Position, &colorSpacePoint );
@@ -152,53 +198,99 @@ int _tmain( int argc, _TCHAR* argv[] )
 									cv::circle( bufferMat, cv::Point( x, y ), 5, static_cast< cv::Scalar >( color[count] ), -1, CV_AA );
 								}
 							}
-						}
+						}*/
 
 						// Set TrackingID to Detect Face
 						UINT64 trackingId = _UI64_MAX;
 						hResult = pBody[count]->get_TrackingId( &trackingId );
 						if( SUCCEEDED( hResult ) ){
-							hResult = pFaceSource->put_TrackingId( trackingId );
-							if( FAILED( hResult ) ){
-								std::cerr << "Error : IFaceFrameSource::put_TrackingId()" << std::endl;
-								return -1;
-							}
+							pFaceSource[count]->put_TrackingId( trackingId );
 						}
 					}
 				}
-				cv::resize( bufferMat, bodyMat, cv::Size(), 0.5, 0.5 );
 			}
 		}
 		SafeRelease( pBodyFrame );
 
-		// Detect Face
-		IFaceFrame* pFaceFrame = nullptr;
-		hResult = pFaceReader->AcquireLatestFrame( &pFaceFrame );
-		if( SUCCEEDED( hResult ) ){
-			IFaceFrameResult* pFaceResult = nullptr;
-			hResult = pFaceFrame->get_FaceFrameResult( &pFaceResult );
-			if( SUCCEEDED( hResult ) ){
-				DWORD feature = FaceFrameFeatures::FaceFrameFeatures_None;
-				hResult = pFaceResult->get_FaceFrameFeatures( &feature );
-				if( SUCCEEDED( hResult ) ){
-					switch( feature )
-					{
-						case FaceFrameFeatures::FaceFrameFeatures_LeftEyeClosed:
-							std::cout << "LeftEyeClosed" << std::endl;
-							break;
-						case FaceFrameFeatures::FaceFrameFeatures_RightEyeClosed:
-							std::cout << "RightEyeClosed" << std::endl;
-							break;
-						default:
-							break;
+		// Face Frame
+		std::system( "cls" );
+		for( int count = 0; count < BODY_COUNT; count++ ){
+			IFaceFrame* pFaceFrame = nullptr;
+			hResult = pFaceReader[count]->AcquireLatestFrame( &pFaceFrame );
+			if( SUCCEEDED( hResult ) && pFaceFrame != nullptr ){
+				BOOLEAN bFaceTracked = false;
+				hResult = pFaceFrame->get_IsTrackingIdValid( &bFaceTracked );
+				if( SUCCEEDED( hResult ) && bFaceTracked ){
+					IFaceFrameResult* pFaceResult = nullptr;
+					hResult = pFaceFrame->get_FaceFrameResult( &pFaceResult );
+					if( SUCCEEDED( hResult ) && pFaceReader != nullptr ){
+						UINT64 trackingId;
+						hResult = pFaceResult->get_TrackingId( &trackingId );
+						if( SUCCEEDED(hResult) ){
+							std::cout << "Count : " << count << ", TrackingID : " << trackingId << std::endl;
+						}
+
+						// Face Bounding Box
+						RectI boundingBox;
+						hResult = pFaceResult->get_FaceBoundingBoxInColorSpace( &boundingBox );
+						if( SUCCEEDED( hResult ) ){
+							cv::rectangle( bufferMat, cv::Rect( boundingBox.Left, boundingBox.Top, boundingBox.Right - boundingBox.Left, boundingBox.Bottom - boundingBox.Top ), static_cast< cv::Scalar >( color[count] ) );
+						}
+
+						// Face Point
+						PointF facePoint[FacePointType::FacePointType_Count];
+						hResult = pFaceResult->GetFacePointsInColorSpace( FacePointType::FacePointType_Count, facePoint );
+						if( SUCCEEDED( hResult ) ){
+							cv::circle( bufferMat, cv::Point( static_cast<int>( facePoint[0].X ), static_cast<int>( facePoint[0].Y ) ), 5, static_cast< cv::Scalar >( color[count] ), -1, CV_AA ); // Eye (Left)
+							cv::circle( bufferMat, cv::Point( static_cast<int>( facePoint[1].X ), static_cast<int>( facePoint[1].Y ) ), 5, static_cast< cv::Scalar >( color[count] ), -1, CV_AA ); // Eye (Right)
+							cv::circle( bufferMat, cv::Point( static_cast<int>( facePoint[2].X ), static_cast<int>( facePoint[2].Y ) ), 5, static_cast< cv::Scalar >( color[count] ), -1, CV_AA ); // Nose
+							cv::circle( bufferMat, cv::Point( static_cast<int>( facePoint[3].X ), static_cast<int>( facePoint[3].Y ) ), 5, static_cast< cv::Scalar >( color[count] ), -1, CV_AA ); // Mouth (Left)
+							cv::circle( bufferMat, cv::Point( static_cast<int>( facePoint[4].X ), static_cast<int>( facePoint[4].Y ) ), 5, static_cast< cv::Scalar >( color[count] ), -1, CV_AA ); // Mouth (Right)
+						}
+
+						// Face Property
+						DetectionResult faceProperty[FaceProperty::FaceProperty_Count];
+						hResult = pFaceResult->GetFaceProperties( FaceProperty::FaceProperty_Count, faceProperty );
+						if( SUCCEEDED( hResult ) ){
+							for( int count = 0; count < FaceProperty::FaceProperty_Count; count++ ){
+								switch( faceProperty[count] ){
+									case DetectionResult::DetectionResult_Unknown:
+										std::cout << property[count] << " : Unknown" << std::endl;
+										break;
+									case DetectionResult::DetectionResult_Yes:
+										std::cout << property[count] << " : Yes" << std::endl;
+										break;
+									case DetectionResult::DetectionResult_No:
+										std::cout << property[count] << " : No" << std::endl;
+										break;
+									case DetectionResult::DetectionResult_Maybe:
+										std::cout << property[count] << " : Mayby" << std::endl;
+										break;
+									default:
+										break;
+								}
+							}
+						}
+
+						// Face Rotation
+						Vector4 faceRotation;
+						hResult = pFaceResult->get_FaceRotationQuaternion( &faceRotation );
+						if( SUCCEEDED( hResult ) ){
+							int pitch, yaw, roll;
+							ExtractFaceRotationInDegrees( &faceRotation, &pitch, &yaw, &roll );
+							std::cout << "Pitch, Yaw, Roll : " << pitch << ", " << yaw << ", " << roll << std::endl;
+						}
+
+						std::cout << std::endl;
 					}
+					SafeRelease( pFaceResult );
 				}
 			}
-			SafeRelease( pFaceResult );
+			SafeRelease( pFaceFrame );
 		}
-		SafeRelease( pFaceFrame );
 
-		cv::imshow( "Face", bodyMat );
+		cv::resize( bufferMat, faceMat, cv::Size(), 0.5, 0.5 );
+		cv::imshow( "Face", faceMat );
 
 		if( cv::waitKey( 10 ) == VK_ESCAPE ){
 			break;
@@ -207,10 +299,14 @@ int _tmain( int argc, _TCHAR* argv[] )
 
 	SafeRelease( pColorSource );
 	SafeRelease( pBodySource );
-	SafeRelease( pFaceSource );
+	for( int count = 0; count < BODY_COUNT; count++ ){
+		SafeRelease( pFaceSource[count] );
+	}
 	SafeRelease( pColorReader );
 	SafeRelease( pBodyReader );
-	SafeRelease( pFaceReader );
+	for( int count = 0; count < BODY_COUNT; count++ ){
+		SafeRelease( pFaceReader[count] );
+	}
 	SafeRelease( pDescription );
 	SafeRelease( pCoordinateMapper );
 	if( pSensor ){
